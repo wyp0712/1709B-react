31089b672d5ed974231c01c4f39afc82ce589791
devinwang <13263262903@163.com>
2020-01-07 08:44:36 +0800

    update

--- 1709b-project-react-te/src/views/Cart.js
+++ 1709b-project-react-te/src/views/Cart.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react'
 
 import RouterView from '../router/index'
 
-import Header from '../components/Header'
-import Footer from '../components/Footer'
+import Header from '@/components/Header'
+import Footer from '@/components/Footer'
 
 export default class componentName extends Component {
   render() {
